5db59fbdfd2b4ffe0611aeefb19731c2c52691c3
Raymond Hulha <java4life@gmail.com>
2017-10-15 03:32:06 +0200

    Initial commit.

---
+++ BackupDBD/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BackupDBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backup = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.buttonOpenTargetDir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backup
+            // 
+            this.backup.Location = new System.Drawing.Point(12, 249);
+            this.backup.Name = "backup";
+            this.backup.Size = new System.Drawing.Size(354, 23);
+            this.backup.TabIndex = 0;
+            this.backup.Text = "Backup Dead by Daylight";
+            this.backup.UseVisualStyleBackColor = true;
+            this.backup.Click += new System.EventHandler(this.backup_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(599, 216);
+            this.textBox.TabIndex = 1;
+            // 
+            // buttonOpenTargetDir
+            // 
+            this.buttonOpenTargetDir.Location = new System.Drawing.Point(384, 249);
+            this.buttonOpenTargetDir.Name = "buttonOpenTargetDir";
+            this.buttonOpenTargetDir.Size = new System.Drawing.Size(227, 23);
+            this.buttonOpenTargetDir.TabIndex = 2;
+            this.buttonOpenTargetDir.Text = "Öffne Backup Ordner";
+            this.buttonOpenTargetDir.UseVisualStyleBackColor = true;
+            this.buttonOpenTargetDir.Click += new System.EventHandler(this.buttonOpenTargetDir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 284);
+            this.Controls.Add(this.buttonOpenTargetDir);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.backup);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Backup DBD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backup;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button buttonOpenTargetDir;
+    }
+}
+
---
+++ BackupDBD/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+namespace BackupDBD
+{
+    public partial class Form1 : Form
+    {
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+        string target;
+
+        public Form1()
+        {
+            InitializeComponent();
+            target = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\BackupDBD";
+            if (!Directory.Exists(target))
+            {
+                Directory.CreateDirectory(target);
+            }
+            if (!Directory.Exists(target))
+            {
+                textBox.AppendText("Konnte Ziel Ordner nicht erstellen: " + target + "\n");
+                backup.Enabled = false;
+            }
+            textBox.AppendText("Backup Ziel: " + target + "\n");
+        }
+
+        private void backup_Click(object sender, EventArgs e)
+        {
+
+            string date = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            textBox.AppendText("Suche Steam Pfad.\n");
+            string steamPath = (string)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Valve\Steam", "SteamPath", null);
+            textBox.AppendText("Steam Pfad hier gefunden: " + steamPath + "\n");
+            string[] profilesDir = Directory.GetDirectories(steamPath+@"\userdata");
+            foreach (string profileDir in profilesDir)
+            {
+                if( Directory.Exists(profileDir + "\\" + 381210))
+                {
+                    string profileID = new DirectoryInfo(profileDir).Name;
+                    textBox.AppendText("User gefunden: " + profileID + "\n");
+                    string sourceDir = profileDir + "\\" + 381210 + "\\remote";
+                    textBox.AppendText("Erstelle DBD Backup für: " + profileID + "\n");
+                    string targetDir = target + "\\" + date + "\\" + profileID + "\\" + 381210 + "\\remote";
+                    Directory.CreateDirectory(targetDir);
+                    DirectoryCopy(sourceDir, targetDir, true);
+                }
+            }
+        }
+
+        private void buttonOpenTargetDir_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", target);
+        }
+    }
+}
